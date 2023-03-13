d3f14bb3b0a03b0c49a3515c429323f334eae98a
muskan000gupta <111727664+muskan000gupta@users.noreply.github.com>
2023-03-14 02:24:27 +0530

    vercel changes

--- server.js
+++ server.js
@@ -78,6 +78,19 @@ io.on('connection', (socket) => {
   });
 });
 
+// Your code
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+          if(err) {
+              res.status(500).send(err)
+          }
+      });
+  })
+}
+// Your code
 
 
 const PORT = process.env.PORT || 5000;
