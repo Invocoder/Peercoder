0f012b5574d280c0e95eb7b44fb142d33bddc92e
Muskan Gupta <51221607+Invocoder@users.noreply.github.com>
2023-03-20 01:40:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -94,6 +94,6 @@ if (process.env.NODE_ENV === "production") {
 // Your code
 
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
