b1fe5d642cc16a23b4ba645e29bc5da6f2e26c90
Muskan Gupta <51221607+Invocoder@users.noreply.github.com>
2023-03-20 01:41:24 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -8,6 +8,6 @@ export const initSocket = async () => {
         timeout: 10000,
         transports: ['websocket'],
     };
-     return io("http://13.49.102.45:6000", options);
+     return io("http://13.49.102.45:5000", options);
 };
 // yarn upgrade codemirror@5.65.12
